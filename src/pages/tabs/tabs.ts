ca76bdf953efb69ccd05a1706ef02d0a676359be
Philip Hayes <philip@deewhy.ie>
2018-03-07 12:15:29 -0500

    added bp main.js

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,7 +3,7 @@ import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { NavController } from 'ionic-angular';
-import { AuthProvider } from '../../providers/auth';
+
 
 @Component({
   templateUrl: 'tabs.html'
@@ -14,18 +14,19 @@ export class TabsPage {
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor(private authProvider: AuthProvider, public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {
   }
   ionViewCanEnter() {
     return new Promise((resolve, reject) => {
-      this.authProvider.isValid()
-      .then( res => {
-        resolve(true);
-      })
-      .catch(err => {
-        this.navCtrl.popToRoot();
-        reject(false);
-      })
+      resolve(true);
+      // this.authProvider.isValid()
+      // .then( res => {
+      //   resolve(true);
+      // })
+      // .catch(err => {
+      //   this.navCtrl.popToRoot();
+      //   reject(false);
+      // })
     });
   }
 }
